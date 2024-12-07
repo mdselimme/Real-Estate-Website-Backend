ea808e4e7cc2240520254f0796f0df6fd419668a
mdselimme <mdselimdev@gmail.com>
2024-12-07 20:29:09 +0600

    add-port-running

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const express = require("express");
 const app = express();
 
 const cors = require("cors");
+
+const port = process.env.PORT || 2000;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Server port is ${port}`);
+});
