f669943beee4b412ece02e05746d8bdd99330245
mdselimme <mdselimdev@gmail.com>
2024-12-21 13:17:16 +0600

    add-mongodb-server

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require("express");
 
+require("dotenv").config();
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const app = express();
 
 const cors = require("cors");
@@ -7,6 +11,38 @@ const cors = require("cors");
 const port = process.env.PORT || 2000;
 
 app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.dopmx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+
+    const database = client.db("HomeLengoRealEstate");
+    const homes = database.collection("homes");
+
+    app.get("/homes", async (req, res) => {
+      const homesdata = homes.find();
+      const result = await homesdata.toArray();
+      res.send(result);
+    });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Server is running");
